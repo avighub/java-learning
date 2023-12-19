e13012c3b83b69c3e23109e719668740a8b327c6
Avishek Behera <avishekbehera87@gmail.com>
2023-12-19 23:39:13 +0530

    add example for pojo

---
+++ src/main/java/pojo/Address.java
@@ -0,0 +1,44 @@
+package pojo;
+
+public class Address {
+
+	private String city;
+	private Integer zip;
+	private String locality;
+
+	public Address(String city, Integer zip, String locality) {
+		this.city = city;
+		this.zip = zip;
+		this.locality = locality;
+	}
+
+	@Override
+	public String toString() {
+		return "Athlete Address: [city=" + this.city + ", zip=" + this.zip + ", locality=" + this.locality + "]";
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Integer getZip() {
+		return zip;
+	}
+
+	public void setZip(Integer zip) {
+		this.zip = zip;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+
+}
---
+++ src/main/java/pojo/CreateAthlete.java
@@ -0,0 +1,80 @@
+package pojo;
+
+public class CreateAthlete {
+
+  /**
+   * defining as private as encapulation to be used only via getter and setter
+   */
+  private String firstName;
+  private String lastName;
+  private String state;
+  private String brmStatus;
+
+  public static void main(String[] args)
+    {
+      CreateAthlete createAthlete = new CreateAthlete("Avishek", "Behera", "Odisha", "SR");
+      System.out.println(createAthlete);
+    }
+
+  public CreateAthlete(String firstName, String lastName, String state, String brmStatus)
+    {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.state = state;
+      this.brmStatus = brmStatus;
+    }
+
+  /**
+   * This will be used to print the Pojo object as String
+   */
+  @Override
+  public String toString()
+    {
+      return "CreateAthlete [firstName=" + firstName + ", lastName=" + lastName + ", state=" + state + ", brmStatus="
+              + brmStatus + "]";
+    }
+
+  /**
+   * Getters and Setters for all private fields
+   */
+  public String getFirstName()
+    {
+      return firstName;
+    }
+
+  public void setFirstName(String firstName)
+    {
+      this.firstName = firstName;
+    }
+
+  public String getLastName()
+    {
+      return lastName;
+    }
+
+  public void setLastName(String lastName)
+    {
+      this.lastName = lastName;
+    }
+
+  public String getState()
+    {
+      return state;
+    }
+
+  public void setState(String state)
+    {
+      this.state = state;
+    }
+
+  public String getBrmStatus()
+    {
+      return brmStatus;
+    }
+
+  public void setBrmStatus(String brmStatus)
+    {
+      this.brmStatus = brmStatus;
+    }
+
+}
