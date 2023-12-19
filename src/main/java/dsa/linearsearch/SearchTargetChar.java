7edea530bb12f6135bc5a240c4bf187f63f0b94f
Avishek Behera <avishekbehera87@gmail.com>
2023-12-19 23:41:43 +0530

    add example for linear search

---
+++ src/main/java/dsa/linearsearch/SearchTargetChar.java
@@ -0,0 +1,45 @@
+package dsa.linearsearch;
+
+public class SearchTargetChar {
+
+  public static void main(String[] args)
+    {
+      char[] characters = {'a', 'f', 'r', 'g', 'b', 'z'};
+      String name = "Avishek";
+      System.out.println("Target element is found at the index: " + findCharacterAndReturnIndex(characters, 'f'));
+      System.out.println("Target element is found at the index: " + findCharacterAndReturnIndex(name.toCharArray(), 'f'));
+      System.out.println("Is Target element found ?: " + isCharacterFound(characters, 'g'));
+      System.out.println("Is Target element found ?: " + isCharacterFound(name.toCharArray(), 'g'));
+      System.out.println("Target element is found at the index: " + findCharacterInRangeAndReturnIndex(characters, 'r', 2, 5));
+    }
+
+  static int findCharacterAndReturnIndex(char[] chars, char targetCharacter)
+    {
+      for (int i = 0; i < chars.length; i++) {
+        if (chars[i] == targetCharacter) {
+          return i;
+        }
+      }
+      return -1; // Returns -1 if target is not found in Array
+    }
+
+  static boolean isCharacterFound(char[] chars, char targetCharacter)
+    {
+      for (char character : chars) {
+        if (character == targetCharacter) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+  static int findCharacterInRangeAndReturnIndex(char[] chars, char targetCharacter, int startRange, int endRange)
+    {
+      for (int i = startRange; i <= endRange; i++) {
+        if (chars[i] == targetCharacter) {
+          return i;
+        }
+      }
+      return -1; // Returns -1 if target is not found in Array
+    }
+}
---
+++ src/main/java/dsa/linearsearch/SearchTargetNumber.java
@@ -0,0 +1,42 @@
+package dsa.linearsearch;
+
+public class SearchTargetNumber {
+
+  public static void main(String[] args)
+    {
+      int[] numbers = {3, 55, 11, 65, 2, 0, 22, 44};
+      System.out.println("Target element is found at the index: " + findNumberAndReturnIndex(numbers, 46));
+      System.out.println("Is Target element found ?: " + isNumberFound(numbers, 11));
+      System.out.println("Target element is found at the index: " + findNumberInRangeAndReturnIndex(numbers, 11, 2, 5));
+    }
+
+  static int findNumberAndReturnIndex(int[] numbers, int targetNumber)
+    {
+      for (int i = 0; i < numbers.length; i++) {
+        if (numbers[i] == targetNumber) {
+          return i;
+        }
+      }
+      return -1; // Returns -1 if target is not found in Array
+    }
+
+  static boolean isNumberFound(int[] numbers, int targetNumber)
+    {
+      for (int number : numbers) {
+        if (number == targetNumber) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+  static int findNumberInRangeAndReturnIndex(int[] numbers, int targetNumber, int startRange, int endRange)
+    {
+      for (int i = startRange; i <= endRange; i++) {
+        if (numbers[i] == targetNumber) {
+          return i;
+        }
+      }
+      return -1; // Returns -1 if target is not found in Array
+    }
+}
