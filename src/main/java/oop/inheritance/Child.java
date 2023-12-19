4e49b4885821097e808f0e9fb02d8fa0932832c7
Avishek Behera <avishekbehera87@gmail.com>
2023-12-20 00:15:43 +0530

    add example for inheritance

---
+++ src/main/java/oop/inheritance/Child.java
@@ -0,0 +1,40 @@
+package oop.inheritance;
+
+public class Child extends Parent {
+
+  public static void main(String[] args)
+    {
+      Child child = new Child();
+      // Print : Child class
+      child.print();
+
+      /* Child class object up cast to super class reference.
+         Using super class reference , sub class object can not use methods
+		 of sub class despite of being object of that class.
+		  Up casting restricts access or visibility of methods downwards */
+      Parent parent = new Parent();
+      parent.print();
+      parent.show();
+
+      /* To access sub class methods, super class reference needs to be downcast to sub class reference. Note here we can down cast
+		to a up cast reference only. */
+
+      Parent parent2 = new Child();
+      Child child2 = (Child) parent2;
+      child2.print();
+      child2.show();
+    }
+
+  @Override
+  public void print()
+    {
+      System.out.println("Child Class Print");
+    }
+
+
+  public void show()
+    {
+      System.out.println("Child Class Show");
+    }
+
+}
---
+++ src/main/java/oop/inheritance/Parent.java
@@ -0,0 +1,11 @@
+package oop.inheritance;
+
+public class Parent {
+  public void print() {
+    System.out.println("Parent Class Print");
+  }
+
+  public void show() {
+    System.out.println("Parent Class Show");
+  }
+}
