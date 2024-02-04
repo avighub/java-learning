f950011823926341548484ade78c6ece7f721b43
Avishek Behera <avishekbehera87@gmail.com>
2024-02-04 22:33:16 +0530

    add programs for StringExercises

--- src/main/java/string/StringExercises.java
+++ src/main/java/string/StringExercises.java
@@ -4,21 +4,42 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class StringExercises {
-  public static void main(String[] args)
-    {
-      printWordsWithEvenLength("Sky is blue and vast");
-    }
+  public static void main(String[] args) {
+    printWordsWithEvenLength("Sky is blue and vast");
 
-  static void printWordsWithEvenLength(String sentence)
-    {
-      // Split string with space and store
-      String[] splitString = sentence.split(" ");
+    System.out.println(getSubstring("world", 2)); //returns substring starting from specified index
+    System.out.println(getIndexOfMatchedString("world", "r")); //returns 2
+    System.out.println(getExtensionNameOfAFile("movie.mp4")); //returns mp4
+    System.out.println(getExtensionNameOfAFileUsingSplit("movie.mp4")); //returns mp4
+  }
 
-      // iterate and find words length and if even print
-      for (String word : splitString) {
-        if (word.length() % 2 == 0) {
-          log.info(word);
-        }
+  static void printWordsWithEvenLength(String sentence) {
+    // Split string with space and store
+    String[] splitString = sentence.split(" ");
+
+    // iterate and find words length and if even print
+    for (String word : splitString) {
+      if (word.length() % 2 == 0) {
+        log.info(word);
       }
     }
+  }
+
+  static String getSubstring(String someString, int index) {
+    return someString.substring(index);
+  }
+
+  static int getIndexOfMatchedString(String someString, String stringToMatch) {
+    return someString.indexOf(stringToMatch);
+  }
+
+  static String getExtensionNameOfAFile(String fileNameWithExtension) {
+    int indexOfDotChar = fileNameWithExtension.indexOf(".");
+    return fileNameWithExtension.substring(indexOfDotChar + 1);
+  }
+
+  static String getExtensionNameOfAFileUsingSplit(String fileNameWithExtension) {
+    String splitStrings[] = fileNameWithExtension.split("\\.");
+    return splitStrings[1];
+  }
 }
