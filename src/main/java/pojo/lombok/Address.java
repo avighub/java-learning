be1637840dbac784c8233a3ef91e7141d897b158
Avishek Behera <avishekbehera87@gmail.com>
2024-03-09 06:05:43 +0530

    update pojo with ignore defaults

--- src/main/java/pojo/lombok/Address.java
+++ src/main/java/pojo/lombok/Address.java
@@ -1,16 +1,18 @@
 package pojo.lombok;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder(setterPrefix = "set")
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Address {
-
   private String city;
   private Integer zip;
   private String locality;
-
 }
